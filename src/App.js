61ee2b3b25bc204ffa3b5a1d0a655efc3c27c3e3
haoxinxin <haoxinxin@haoxinxindeiMac.local>
2018-08-27 17:08:01 +0800

    设置地图中心

--- src/App.js
+++ src/App.js
@@ -89,6 +89,7 @@ class App extends Component {
     this.createInfoWindow = this.createInfoWindow.bind(this);
     this.closeInfoWindow = this.closeInfoWindow.bind(this);
     this.showMarkerInfoWindow = this.showMarkerInfoWindow.bind(this);
+    this.getMarkerInfo = this.getMarkerInfo.bind(this);
     // this.handleSetMarker = this.handleSetMarker.bind(this);
   }
 
@@ -98,7 +99,7 @@ class App extends Component {
     mapview.style.height = window.innerHeight + "px";
     this.setState({
       map: new window.AMap.Map('map-container', {
-        center: [116.397428, 39.90923],
+        center: [116.397612, 39.909249],
         zoom: 11
       })
     })
@@ -123,13 +124,18 @@ class App extends Component {
 
       let infoWindow = new window.AMap.InfoWindow({
         isCustom: true,  //使用自定义窗体
-        content: this.createInfoWindow(location.name, content.join("")),
+        content: this.createInfoWindow(marker,location.name, content.join("")),
         offset: new window.AMap.Pixel(16, -45)
       });
 
       location.infoWindow = infoWindow;
 
       window.AMap.event.addListener(marker, 'click', function () {
+        let pos = marker.getPosition();
+        let position = new window.AMap.LngLat(pos.lng,pos.lat);  // 标准写法
+        console.log(position);
+        self.state.map.setCenter(position);
+        console.log('center',self.state.map.getCenter())
         infoWindow.open(self.state.map, marker.getPosition());
       });
 
@@ -141,9 +147,9 @@ class App extends Component {
     })
   }
 
-  createInfoWindow (title, content) {
+  createInfoWindow (marker, title, content) {
     this.state.map.clearInfoWindow();
-    var info = document.createElement("div");
+    let info = document.createElement("div");
     info.className = "info";
 
     //可以通过下面的方式修改自定义窗体的宽高
@@ -210,6 +216,36 @@ class App extends Component {
     console.log('componentDidMount')
     window.initMap = this.initMap;
     addMapScript("https://webapi.amap.com/maps?v=1.4.8&key=0b090011d74899419909ebb28322486e&callback=initMap");
+  }
+
+  getMarkerInfo(marker) {
+    var self = this;
+    var clientId = "TPIDDHBKB2QFBWEV2MPDOFGUSWXCXGAA5IVOWEMN5ASR3UJW";
+    var clientSecret = "4HB1ZZJBVXC3F0BREBPSGXYK0VZ5ALS4XRNJZSBP1JROG0DE";
+    var url = "https://api.foursquare.com/v2/venues/search?client_id=" + clientId + "&client_secret=" + clientSecret + "&v=20130815&ll=" + marker.getPosition().lat() + "," + marker.getPosition().lng() + "&limit=1";
+    fetch(url)
+      .then(
+        function (response) {
+          if (response.status !== 200) {
+            self.state.infowindow.setContent("Sorry data can't be loaded");
+            return;
+          }
+
+          // Examine the text in the response
+          response.json().then(function (data) {
+            var location_data = data.response.venues[0];
+            var verified = '<b>Verified Location: </b>' + (location_data.verified ? 'Yes' : 'No') + '<br>';
+            var checkinsCount = '<b>Number of CheckIn: </b>' + location_data.stats.checkinsCount + '<br>';
+            var usersCount = '<b>Number of Users: </b>' + location_data.stats.usersCount + '<br>';
+            var tipCount = '<b>Number of Tips: </b>' + location_data.stats.tipCount + '<br>';
+            var readMore = '<a href="https://foursquare.com/v/'+ location_data.id +'" target="_blank">Read More on Foursquare Website</a>'
+            self.state.infowindow.setContent(checkinsCount + usersCount + tipCount + verified + readMore);
+          });
+        }
+      )
+      .catch(function (err) {
+        self.state.infowindow.setContent("Sorry data can't be loaded");
+      });
   }
 
   render () {
