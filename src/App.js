2b5395d41fbd80bcd937f00dbc1da706fde95641
haoxinxin <haoxinxin@haoxinxindeiMac.local>
2018-08-27 16:32:19 +0800

    添加markder

--- src/App.js
+++ src/App.js
@@ -9,74 +9,74 @@ class App extends Component {
   state = {
     locationList: [
       {
-        'name': "Bhaskar Vidhya Ashram",
-        'type': "Private School",
-        'latitude': 26.9053803,
-        'longitude': 75.7259351,
-        'streetAddress': "Lalarpura Road, Gandhi Path"
+        'name': "北京机场",
+        'type': "公共设施",
+        'latitude': 116.604412,
+        'longitude': 40.071234,
+        'streetAddress': "北京市朝阳区首都机场路"
       },
       {
-        'name': "Hotel Chhavi Holidays",
-        'type': "Hotel",
-        'latitude': 26.9055311,
-        'longitude': 75.728137,
-        'streetAddress': "Plot No. 11/12, Vivek Vihar"
+        'name': "天安门广场",
+        'type': "旅游景点",
+        'latitude': 116.397612,
+        'longitude': 39.909249,
+        'streetAddress': "北京市东城区东长安街"
       },
       {
-        'name': "Handi",
-        'type': "Restaurant",
-        'latitude': 26.906990,
-        'longitude': 75.742848,
-        'streetAddress': "18, Gautam Marg, Vaishali Nagar"
+        'name': "朝阳公园",
+        'type': "公共设施",
+        'latitude': 116.482413,
+        'longitude': 39.944265,
+        'streetAddress': "北京市朝阳区朝阳公园南路1号"
       },
       {
-        'name': "INOX - Amrapali",
-        'type': "Movie Theater",
-        'latitude': 26.912631,
-        'longitude': 75.743389,
-        'streetAddress': "C-1, Vaibhav Complex"
+        'name': "三源里菜市场",
+        'type': "公共设施",
+        'latitude': 116.456985,
+        'longitude': 39.9511,
+        'streetAddress': "北京市朝阳区顺源里2号楼东侧"
       },
       {
-        'name': "Blue Dart",
-        'type': "Courier Service",
-        'latitude': 26.911103,
-        'longitude': 75.738878,
-        'streetAddress': "Vaishali Tower, Vaishali Nagar"
+        'name': "首都体育馆",
+        'type': "公共设施",
+        'latitude': 116.326265,
+        'longitude': 39.939116,
+        'streetAddress': "北京市海淀区中关村南大街56号"
       },
       {
-        'name': "Hotel Seven Seas",
-        'type': "3-Star Hotel",
-        'latitude': 26.906069,
-        'longitude': 75.739583,
-        'streetAddress': "A-6, Nemi Nagar, Gandhi Path"
+        'name': "北京大学",
+        'type': "教育设施",
+        'latitude': 116.310644,
+        'longitude': 39.992663,
+        'streetAddress': "北京市海淀区颐和园路5号"
       },
       {
-        'name': "Global Heart & General Hospital",
-        'type': "Hospital",
-        'latitude': 26.905506,
-        'longitude': 75.738762,
-        'streetAddress': "C1/27, Opposite Bharat Apartment"
+        'name': "清华大学",
+        'type': "教育设施",
+        'latitude': 116.325407,
+        'longitude': 40.002526,
+        'streetAddress': "北京市海淀区双清路30号"
       },
       {
-        'name': "Shri Swaminarayan Mandir",
-        'type': "Hindu Temple",
-        'latitude': 26.902167,
-        'longitude': 75.740999,
-        'streetAddress': "Sector 9, Chitrakoot"
+        'name': "玉渊潭公园",
+        'type': "公共设施",
+        'latitude': 116.31957,
+        'longitude': 39.916079,
+        'streetAddress': "北京市海淀区西三环中路10号"
       },
       {
-        'name': "Pratap Marriage Garden",
-        'type': "Banquet Hall",
-        'latitude': 26.906464,
-        'longitude': 75.732889,
-        'streetAddress': "Arpit Nagar, B Block"
+        'name': "北京西站",
+        'type': "公共设施",
+        'latitude': 116.32266,
+        'longitude': 39.89422,
+        'streetAddress': "北京市丰台区莲花池东路118号"
       },
       {
-        'name': "ICICI Bank",
-        'type': "Bank",
-        'latitude': 26.913179,
-        'longitude': 75.743447,
-        'streetAddress': "Lalarpura Road, Gandhi Path"
+        'name': "银泰百货",
+        'type': "商场",
+        'latitude': 116.384458,
+        'longitude': 39.843363,
+        'streetAddress': "北京市丰台区马家堡东路101号院10号楼"
       }
     ]
   }
@@ -84,14 +84,120 @@ class App extends Component {
   constructor (props) {
     super(props);
     this.handleSearchChange = this.handleSearchChange.bind(this);
+    this.initMap = this.initMap.bind(this);
+    this.setMarker = this.setMarker.bind(this);
+    this.createInfoWindow = this.createInfoWindow.bind(this);
+    this.closeInfoWindow = this.closeInfoWindow.bind(this);
+    this.showMarkerInfoWindow = this.showMarkerInfoWindow.bind(this);
+    // this.handleSetMarker = this.handleSetMarker.bind(this);
   }
 
   initMap () {
-    console.log('init Map')
-    new window.AMap.Map('map-container', {
-      center: [26.907502, 75.737586],
-      zoom: 11
+    let self = this;
+    var mapview = document.getElementById('map-container');
+    mapview.style.height = window.innerHeight + "px";
+    this.setState({
+      map: new window.AMap.Map('map-container', {
+        center: [116.397428, 39.90923],
+        zoom: 11
+      })
     })
+
+    let alllocations = [];
+
+    //初始化markers
+    this.state.locationList.forEach((location) => {
+      let marker = new window.AMap.Marker({
+        position: new window.AMap.LngLat(location.latitude, location.longitude),
+        title: location.name,
+        visible: true,
+        clickable: true,
+        map: this.state.map
+      })
+      location.marker = marker;
+
+      // 设置marker点击事件
+      let content = [];
+      content.push(`<p>类型: ${location.type}</p>`)
+      content.push(`<p>地址: ${location.streetAddress}</p>`)
+
+      let infoWindow = new window.AMap.InfoWindow({
+        isCustom: true,  //使用自定义窗体
+        content: this.createInfoWindow(location.name, content.join("")),
+        offset: new window.AMap.Pixel(16, -45)
+      });
+
+      location.infoWindow = infoWindow;
+
+      window.AMap.event.addListener(marker, 'click', function () {
+        infoWindow.open(self.state.map, marker.getPosition());
+      });
+
+      alllocations.push(location);
+    })
+
+    this.setState({
+      locationList: alllocations
+    })
+  }
+
+  createInfoWindow (title, content) {
+    this.state.map.clearInfoWindow();
+    var info = document.createElement("div");
+    info.className = "info";
+
+    //可以通过下面的方式修改自定义窗体的宽高
+    //info.style.width = "400px";
+    // 定义顶部标题
+    var top = document.createElement("div");
+    var titleD = document.createElement("div");
+    var closeX = document.createElement("img");
+    top.className = "info-top";
+    titleD.innerHTML = title;
+    closeX.src = "https://webapi.amap.com/images/close2.gif";
+    closeX.onclick = this.closeInfoWindow;
+
+    top.appendChild(titleD);
+    top.appendChild(closeX);
+    info.appendChild(top);
+
+    // 定义中部内容
+    var middle = document.createElement("div");
+    middle.className = "info-middle";
+    middle.style.backgroundColor = 'white';
+    middle.innerHTML = content;
+    info.appendChild(middle);
+
+    // 定义底部内容
+    var bottom = document.createElement("div");
+    bottom.className = "info-bottom";
+    bottom.style.position = 'relative';
+    bottom.style.top = '0px';
+    bottom.style.margin = '0 auto';
+    var sharp = document.createElement("img");
+    sharp.src = "https://webapi.amap.com/images/sharp.png";
+    bottom.appendChild(sharp);
+    info.appendChild(bottom);
+    return info;
+  }
+
+  closeInfoWindow () {
+    this.state.map.clearInfoWindow();
+  }
+
+  setMarker (index, visible) {
+    let location = this.state.locationList;
+    if (!visible) {
+      this.state.map.remove(location[index].marker)
+    } else {
+      this.state.map.add(location[index].marker)
+    }
+  }
+
+  showMarkerInfoWindow (loc) {
+    let index = this.state.locationList.indexOf(loc);
+    console.log(loc,index)
+    loc.infoWindow.open(this.state.map, loc.marker.getPosition());
   }
 
   handleSearchChange (val) {
@@ -109,7 +215,9 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <AsideLists alllocations={this.state.locationList}/>
+        <AsideLists alllocations={this.state.locationList}
+                    setMarker={this.setMarker}
+                    showMarkerWindow={this.showMarkerInfoWindow}/>
         <div id="map-container"></div>
       </div>
     );
--- src/components/AsideLists.js
+++ src/components/AsideLists.js
@@ -3,8 +3,32 @@ import React, {Component} from 'react';
 class AsideLists extends Component {
   state = {
     show: true,
+    showLists: true,
     filterlocations: [],
     searchValue: ''
+  }
+
+  constructor (props) {
+    super(props);
+    this.handleVisibleSearch = this.handleVisibleSearch.bind(this);
+    this.showMarkerWindow = this.showMarkerWindow.bind(this);
+    this.handleShowLists = this.handleShowLists.bind(this);
+  }
+
+  handleVisibleSearch (e) {
+    this.setState({
+      show: !this.state.show
+    })
+  }
+
+  handleShowLists () {
+    this.setState({
+      showLists: !this.state.showLists
+    })
+  }
+
+  showMarkerWindow (e, loc) {
+    this.props.showMarkerWindow(loc);
   }
 
   funcFilterLocation = (e) => {
@@ -12,7 +36,12 @@ class AsideLists extends Component {
     let locations = [];
     this.props.alllocations.forEach((loc, index) => {
       if (loc.name.toLowerCase().indexOf(value.toLowerCase()) !== -1) {
+        // 添加mark
+        this.props.setMarker(index, true);
         locations.push(loc);
+      } else {
+        // 删除mark
+        this.props.setMarker(index, false);
       }
     })
     this.setState({
@@ -28,21 +57,33 @@ class AsideLists extends Component {
   }
 
   render () {
+    let ulStyle = !this.state.show ? {display: "none"} : {};
+    let listStyle = !this.state.showLists ? {display: "block"} : {}
     return (
       <aside className="location-list">
-        <input type="text" className="location-list-input"
-               value={this.state.searchValue}
-               onChange={this.funcFilterLocation}/>
-        <ul className="location-list-items">
-          {
-            this.state.filterlocations
-              .map((loc) => {
-                return (
-                  <li key={loc.name} className="location-list-item">{loc.name}</li>
-                )
-              })
-          }
-        </ul>
+        <div className="location-nav font-icon"
+             onClick={this.handleShowLists}>&#xe62b;</div>
+        <div className="location-list-content" style={listStyle}>
+          <input type="text"
+                 className="location-list-input"
+                 value={this.state.searchValue}
+                 onChange={this.funcFilterLocation}/>
+          <ul className="location-list-items"
+              style={ulStyle}>
+            {
+              this.state.filterlocations
+                .map((loc) => {
+                  return (
+                    <li key={loc.name}
+                        className="location-list-item"
+                        onClick={(e) => this.showMarkerWindow(e, loc)}>{loc.name}</li>
+                  )
+                })
+            }
+          </ul>
+          <a className="show-btn"
+             onClick={this.handleVisibleSearch}>{this.state.show ? "隐藏" : "显示"}搜索</a>
+        </div>
       </aside>
     )
   }
